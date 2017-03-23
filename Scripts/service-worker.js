d678921e34e4f464e8080e56f60b8d5190909feb
Qinghua Wang <qhwang@hengtiansoft.com>
2017-03-22 17:19:38 -0700

    no message

--- Scripts/service-worker.js
+++ Scripts/service-worker.js
@@ -1,44 +1,7 @@
-﻿"use strict";
+﻿
+// Claim clients so that the very first page load is controlled by a service
+// worker. (Important for responding correctly in offline state.)
+self.addEventListener('activate', () => self.clients.claim());
 
-console.log('WORKER: executing.');
-
-/* A version number is useful when updating the worker logic,
-   allowing you to remove outdated cache entries during the update.
-*/
-var version = 'v1::';
-
-/* These resources will be downloaded and cached by the service worker
-   during the installation process. If any resource fails to be downloaded,
-   then the service worker won't be installed either.
-*/
-
-/* The install event fires when the service worker is first installed.
-   You can use this event to prepare the service worker to be able to serve
-   files while visitors are offline.
-*/
-self.addEventListener("install", function (event) {
-    console.log('WORKER: install event in progress.');
-});
-
-/* The fetch event fires whenever a page controlled by this service worker requests
-   a resource. This isn't limited to `fetch` or even XMLHttpRequest. Instead, it
-   comprehends even the request for the HTML page on first load, as well as JS and
-   CSS resources, fonts, any images, etc.
-*/
-self.addEventListener("fetch", function (event) {
-    console.log('WORKER: fetch event in progress.');
-});
-
-/* The activate event fires after a service worker has been successfully installed.
-   It is most useful when phasing out an older version of a service worker, as at
-   this point you know that the new worker was installed correctly. In this example,
-   we delete old caches that don't match the version in the worker we just finished
-   installing.
-*/
-self.addEventListener("activate", function (event) {
-    /* Just like with the install event, event.waitUntil blocks activate on a promise.
-       Activation will fail unless the promise is fulfilled.
-    */
-    console.log('WORKER: activate event in progress.');
-
-});
+// Make sure the SW the page we register() is the service we use.
+self.addEventListener('install', () => self.skipWaiting());
